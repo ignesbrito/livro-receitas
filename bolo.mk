61c12c82830cf6b32db90c42768842ea750338e3
Ignes <ignes89@gmial.com>
2021-01-31 22:52:56 -0300

    commit inicial

---
+++ bolo.mk
@@ -0,0 +1,27 @@
+# Bolo de Laranja :cake:
+
+**Ingredientes: **
+
+- 4 ovos
+
+- 2 xícaras (chá) de açúcar
+
+- 1 xícara (chá) de óleo
+
+- suco de 2 laranjas
+
+- casca de 1 laranja
+
+- 2 xícaras (chá) [de farinha de trigo](https://blog.tudogostoso.com.br/cardapios/tipos-de-farinha-de-trigo-diferencas-entre-elas/)
+
+- 1 colher (sopa) de fermento
+
+**Modo de preparo**
+
+Bata no liquidificador os ovos, o açúcar, o óleo, o suco e a casca da laranja.
+
+Passe para uma tigela e acrescente a farinha de trigo e o fermento.
+
+Leve para assar em uma forma com furo central, [untada e enfarinhada](https://blog.tudogostoso.com.br/dicas-de-cozinha/como-untar-a-forma-do-bolo/), por mais ou menos 30 minutos.
+
+Desenforme o bolo e molhe com suco de laranja.
